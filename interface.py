4fe2340354e813380314a0646a2bd9f0b6e82c21
Phil Hobbs <philhobbs@hotmail.com>
2015-02-02 18:14:36 +0000

    Moved Object List to File
    List of trained objects now in csv file

--- interface.py
+++ interface.py
@@ -1,11 +1,20 @@
 from Tkinter import *
 import objDetect
+import tkMessageBox
+import csv
 
 gui = Tk()
 
 def searchForObject():
     obj = select_li.get(select_li.curselection())
     objDetect.displayMatch(obj)
+
+
+def processFile():
+	with open('objects.csv', 'rb') as csv_file:
+		reader = csv.reader(csv_file, delimiter=' ')
+		for row in reader:
+			print row
 
 # Set up label
 lbl_txt = StringVar()
@@ -15,17 +24,21 @@ lbl_txt.set("Select object to find:")
 # Set up list box
 select_li = Listbox(gui)
 select_li.insert(1, "strep")
-select_li.insert(2, "id")
-select_li.insert(3, "sid")
+select_li.insert(2, "staff")
+select_li.insert(3, "student")
 select_li.insert(4, "passport")
-select_li.insert(5, "IT Card")
+select_li.insert(5, "card")
 
 # Set up search button
 search_btn = Button(gui, text="Search", command=searchForObject)
 
+# Button to process file
+process_btn = Button(gui, text="Process File", command=processFile)
+
 search_lbl.pack()
 select_li.pack()
 search_btn.pack()
+process_btn.pack()
 
 gui.mainloop()
 
