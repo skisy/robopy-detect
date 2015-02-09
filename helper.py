245231a672e4a4b8910c8cc7fff05418b4fd7563
Phil Hobbs <philhobbs@hotmail.com>
2015-02-09 01:38:10 +0000

    Cropping and Print Robot Commands
    - Cropping functionality full implemented: User can select existing
    image, crop around object and file is saved to trainImg directory
    
    - Untested function to print out potential robot commands (directions)
    based on location of object in image

--- helper.py
+++ helper.py
@@ -21,13 +21,12 @@ def cropImage(image_path):
 	img = cv2.imread(image_path)
 	title = "Crop Image"
 
-	## TEST CODE
-	global current_pos
+	global cursor_pos
 	global top_left
 	global bottom_right
 	global released_once
 
-	current_pos = None
+	cursor_pos = None
 	top_left = None
 	bottom_right = None
 	released_once = False
@@ -35,35 +34,36 @@ def cropImage(image_path):
 	cv2.namedWindow(title)
 
 	def onMouse(event, x, y, flags, param):
-		global current_pos
+		global cursor_pos
 		global top_left
 		global bottom_right
 		global released_once
 
-		current_pos = (x, y)
+		cursor_pos = (x, y)
 
 		if top_left is not None and not (flags & cv2.EVENT_FLAG_LBUTTON):
 			released_once = True
 
 		if flags & cv2.EVENT_FLAG_LBUTTON:
 			if top_left is None:
-				top_left = current_pos
+				top_left = cursor_pos
 			elif released_once:
-				bottom_right = current_pos
+				bottom_right = cursor_pos
 
 	cv2.setMouseCallback(title, onMouse)
 	cv2.imshow("Crop Image",img)
 
 	while bottom_right is None:
-		print current_pos
 		im_draw = np.copy(img)
 
 		if top_left is not None:
-			cv2.rectangle(im_draw, top_left, current_pos, (255, 0, 0))
+			cv2.rectangle(im_draw, top_left, cursor_pos, (255, 0, 0))
 
 		cv2.imshow(title, im_draw)
 		_ = cv2.waitKey(10)
 
-		# @TODO Save cropped image
+	cv2.destroyWindow(title)
 
-	
+	(width, height) = np.subtract(bottom_right,top_left)
+	cropped = img[top_left[1]:top_left[1]+height,top_left[0]:top_left[0]+width]
+	cv2.imwrite(image_path,cropped)
